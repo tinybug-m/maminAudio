2dc4d1f3717ee65f027b6b8bf4ae741bb9eca42e
Ranjbar.Arshia <Ranjbar.Arshia@Okala.Com>
2025-08-08 14:57:38 +0330

    fix: absolute paths (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import LandingPage from "./landingPage/LandingPage";
+import LandingPage from "@/landingPage/LandingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AboutPage from "./about/AboutPage";
-import Contnact from "./contact/ContactPage";
+import AboutPage from "@/about/AboutPage";
+import Contnact from "@/contact/ContactPage";
 import { ThemeProvider } from "@mui/material/styles";
 
-import * as React from "react";
-
-import AppAppBar from "./landingPage/components/AppAppBar";
+import AppAppBar from "@/landingPage/components/AppAppBar";
 import CssBaseline from "@mui/material/CssBaseline";
-import Loading from "./landingPage/components/universal/Loading";
-import { defaultTheme } from "./themes";
+import Loading from "@/landingPage/components/universal/Loading";
+import { defaultTheme } from "@/themes";
 
 function App() {
   const [mode, setMode] = React.useState("light");
