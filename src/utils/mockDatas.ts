8c64d875c6f44e1402c1c0679842d793ed417ffa
Ranjbar.Arshia <Ranjbar.Arshia@Okala.Com>
2025-08-09 17:20:53 +0330

    feat: add WORKS_MOCK_DATA constant to utils/mockDatas.ts

---
+++ src/utils/mockDatas.ts
@@ -0,0 +1,104 @@
+export const WORKS_MOCK_DATA = [
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: [
+      "Motion Graphic",
+      "Animation",
+      "Animation",
+      "Animation",
+      "Animation",
+    ],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+  {
+    img: "/static/images/cards/chi.png",
+    video: "",
+    title: "Lizard",
+    tags: ["Motion Graphic", "Animation"],
+    description:
+      "its gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxtits gonna be a long long texxt",
+  },
+];
